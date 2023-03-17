115ac68907f75b8caee8f8fee57b2a09f7f6ba53
shilpa biradar <108936323+shilpabiradar100@users.noreply.github.com>
2023-03-17 23:19:43 +0530

    Delete StartPosition.java

--- src/StartPosition.java
+++
@@ -1,9 +0,0 @@
-
-/*Snake and Ladder game played with single player at start position 0*/
-
-public class StartPosition {
-    public static void main(String[] args) {
-        int playerPosition = 0;
-        System.out.println("Position of player is " + playerPosition);
-    }
-}
