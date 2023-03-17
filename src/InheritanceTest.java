23bea6f6c94eb04e961022d1c2a3d18e78a01a93
shilpa123 <shilpabiradar100@gmail.com>
2023-03-17 23:16:25 +0530

    LineComaparisionProblem

---
+++ src/InheritanceTest.java
@@ -0,0 +1,5 @@
+public class InheritanceTest {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/StartPosition.java
@@ -0,0 +1,9 @@
+
+/*Snake and Ladder game played with single player at start position 0*/
+
+public class StartPosition {
+    public static void main(String[] args) {
+        int playerPosition = 0;
+        System.out.println("Position of player is " + playerPosition);
+    }
+}
